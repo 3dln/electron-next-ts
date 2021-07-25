42211376d67942d9e6206db06ebb51e454e9de1e
Ashkan Ashtiani <ashcan@3dln.com>
2021-07-25 15:23:51 +0430

    tailwindcss

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useEffect } from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   useEffect(() => {
     // add a listener to 'message' channel
-    global.ipcRenderer.addListener('message', (_event, args) => {
-      alert(args)
-    })
-  }, [])
+    global.ipcRenderer.addListener("message", (_event, args) => {
+      alert(args);
+    });
+  }, []);
 
   const onSayHiClick = () => {
-    global.ipcRenderer.send('message', 'hi from next')
-  }
+    global.ipcRenderer.send("message", "hi from next");
+  };
 
   return (
     <Layout title="Home | Next.js + TypeScript + Electron Example">
-      <h1>Hello Next.js 👋</h1>
+      <h1 className="bg-red-200">Hello Next.js 👋</h1>
       <button onClick={onSayHiClick}>Say hi to electron</button>
       <p>
         <Link href="/about">
@@ -24,7 +24,7 @@ const IndexPage = () => {
         </Link>
       </p>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
