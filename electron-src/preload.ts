1744525007d74a282c64247e5274d24165a6fccf
Ashkan Ashtiani <ashcan@3dln.com>
2021-07-25 15:04:32 +0430

    updated packages

--- electron-src/preload.ts
+++ electron-src/preload.ts
@@ -1,17 +1,13 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ipcRenderer, IpcRenderer } from 'electron'
+import { ipcRenderer, IpcRenderer } from "electron";
 
-declare global {
-  namespace NodeJS {
-    interface Global {
-      ipcRenderer: IpcRenderer
-    }
-  }
+declare module globalThis {
+  let ipcRenderer: IpcRenderer;
 }
 
 // Since we disabled nodeIntegration we can reintroduce
 // needed node functionality here
-process.once('loaded', () => {
-  global.ipcRenderer = ipcRenderer
-})
+process.once("loaded", () => {
+  globalThis.ipcRenderer = ipcRenderer;
+});
